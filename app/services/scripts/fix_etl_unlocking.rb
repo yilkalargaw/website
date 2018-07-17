000886d038122e5286326d95e7c9d9c8959e89c6
Jeremy Walker <jez.walker@gmail.com>
2018-07-17 16:00:45 +0100

    Fix ETL unlocking script

--- app/services/scripts/fix_etl_unlocking.rb
+++ app/services/scripts/fix_etl_unlocking.rb
@@ -8,26 +8,45 @@ module Scripts
       end
     end
 
-    # Note: This doesn't take into consideration exercises 
-    # completed without approval. That should be added before 
+    # Note: This doesn't take into consideration exercises
+    # completed without approval. That should be added before
     # this is run in the future.
     def fix_track(track)
+      core_exercises = track.exercises.core.order("position ASC")
+      core_exercise_ids = core_exercises.map(&:id)
       side_exercises = track.exercises.side
-      unlocked_by, auto_unlock = side_exercises.partition { |e| e.unlocked_by_id }
+      unlocked_by, auto_unlock = side_exercises.partition { |e| e.unlocked_by_id.present? }
 
       UserTrack.includes(:user).where(track_id: track.id).each do |ut|
         user = ut.user
 
+        # Get core exercises for checking
+        existing_core_ids = Solution.joins(:exercise).where('exercises.core': true).where(user_id: ut.user_id, "exercises.track_id": ut.track_id).pluck(:exercise_id)
+
+        # Unlock the first core if appropriate
+        # Do this before getting the other stuff
+        if (core_exercise_ids & existing_core_ids).empty?
+          CreatesSolution.create!(user, core_exercises.first)
+        end
+
+        # Get side exercises for checking
         existing_completed_exercise_ids = Solution.joins(:exercise).where(user_id: ut.user_id, "exercises.track_id": ut.track_id).completed.pluck(:exercise_id)
         existing_uncompleted_exercise_ids = Solution.joins(:exercise).where(user_id: ut.user_id, "exercises.track_id": ut.track_id).not_completed.pluck(:exercise_id)
         existing_exercise_ids = existing_completed_exercise_ids + existing_uncompleted_exercise_ids
 
+        # Unlock the first core if appropriate
+        if (core_exercise_ids & existing_exercise_ids).empty?
+          CreatesSolution.create!(user, core_exercises.first)
+        end
+
+        # Unlock auto unlocks
         auto_unlock.each do |side_exercise|
           unless existing_exercise_ids.include?(side_exercise.id)
             CreatesSolution.create!(user, side_exercise)
           end
         end
 
+        # Unlock unlocked side exercsies
         unlocked_by.each do |side_exercise|
           if !existing_exercise_ids.include?(side_exercise.id) &&
              existing_completed_exercise_ids.include?(side_exercise.unlocked_by_id)
